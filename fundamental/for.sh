d4c1da4e0fed0b7daef3117fd87a87d57dbb23ca
huangyingw@linux_programming <huangyingw@gmail.com>
2010-01-27 01:45:47 +0800

    Show all of the realization of for.sh file

--- fundamental/for.sh
+++ fundamental/for.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
+IFS=$'\n'
+
 dir=/root/myproject/git/folder/
+
 find "${dir}" -type f | while read file ; do
    echo "prefix${file}"
 done
+
+echo $IFS
+
+find $dir -type f -exec echo prefix'{}' \;
 
 for ((i = 0;i<=5;i++))
 do
