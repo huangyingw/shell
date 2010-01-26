2d0ebb1f8430bb51d52b7319a756ffbee0f7c140
huangyingw@linux_programming <huangyingw@gmail.com>
2010-01-27 04:57:20 +0800

    no

--- fundamental/back.sh
+++ fundamental/back.sh
@@ -4,5 +4,6 @@ lv_file=/root/myproject/git/linux/shell/fundamental/log.txt
 lvdisplay | grep -o "/dev.*" > $lv_file
 
 cat $lv_file | while read file ; do
-   echo "$file"
+	echo "$file"
+	echo "$file" | sed 's/dev/media/'
 done
