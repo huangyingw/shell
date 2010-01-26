c56040483e0d0e2d4a319251178c56a5d2878e4c
huangyingw@linux_programming <huangyingw@gmail.com>
2010-01-27 02:48:27 +0800

    test.sh works

--- fundamental/mklv.sh
+++ fundamental/mklv.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 dir=/home/huangyingw/
-for file in "`cat /root/myproject/git/linux/shell/fundamental/lv.txt`";
+
+for file in `cat /root/myproject/git/linux/shell/fundamental/lv.txt`;
 do 
+		#echo /media/volgrp/${file}
 		mkdir -p /media/volgrp/${file}
+		
 done
