b2b1bd32419a3cdd5752445e3288bc21847aabb2
huangyingw@linux_programming <huangyingw@gmail.com>
2010-01-27 01:38:30 +0800

    Use the while loop to replace the for loop

--- fundamental/for.sh
+++ fundamental/for.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 dir=/root/myproject/git/folder/
-find $dir -type f -exec echo prefix'{}' \;
+find "${dir}" -type f | while read file ; do
+   echo "prefix${file}"
+done
 
 for ((i = 0;i<=5;i++))
 do
