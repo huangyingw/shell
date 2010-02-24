d0eedcf3547ae3a9f8a5eb21984e40047abbcdb2
huangyingw@linux_programming <huangyingw@gmail.com>
2010-02-24 21:17:20 +0800

    no

--- fundamental/move_with_key.sh
+++ fundamental/move_with_key.sh
@@ -2,13 +2,4 @@
 RSYNCSOURCE=$1
 RSYNCTARGET=$2
 
-if [ -d "${RSYNCTARGET}" ];   
-then   
-    echo dir exists
-    exit
-else   
- 		rsync -av --recursive --delete -h --times --links --hard-links \
-			--stats --progress \
-			"${RSYNCSOURCE}" "${RSYNCTARGET}" \
-		&& rm -frv "${RSYNCSOURCE}"
-fi
+find / -type f -name "tutorial.html"> ${script_path}/find.log
