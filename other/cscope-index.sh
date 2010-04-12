9a82ae2a16c9bcbd4f022ebb7beb3fe175699060
huangyingw@linux_programming <huangyingw@gmail.com>
2010-04-12 03:34:30 -0400

    n

--- other/cscope-index.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find /media/storage/linux/src/ -name "*.h" -o -name "*.c" -o -name "*.cc" > /media/storage/linux/src/cscope.files
-cd /media/storage/linux/src/
-cscope -bq -i /media/storage/linux/src/cscope.files
-ctags -R
-vim /media/storage/linux/src/linux-2.4/init/main.c
