b7a955fefdca315fac32865be968faadea88e8a0
huangyingw@linux_programming <huangyingw@gmail.com>
2010-04-18 10:42:44 -0400

    n

--- fundamental/case.sh
+++ fundamental/case.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
-lv_file=/root/myproject/git/linux/shell/fundamental/log.txt
-lvdisplay | grep -o "/dev.*" > $lv_file
-
-cat $lv_file | while read file ; do
-	dir=`echo "$file" | sed 's/dev/media/'`
-	mkdir -p $dir
-	#mkdir -p 'echo "$file" | sed 's/dev/media/''
-done
+case "$1" in
+	start)
+		echo "hello"
+		;;
+esac
