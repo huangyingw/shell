cd4171dd6c65a459667ad3b70b3672ce12bce659
huangyingw@linux_programming <huangyingw@gmail.com>
2010-05-09 11:08:21 +0800

    n

--- fundamental/iso.sh
+++ fundamental/iso.sh
@@ -4,10 +4,9 @@ source=/home/huangyingw/GBTD-001\]\[Players.GameAoi\ Sora.ISO
 #source=/media/storage/primaryStorage/xgmm/A4U][Asian4You][A4Y]Photos.CD/amara11z005.jpg
 target=/home/huangyingw/folder
 
-#mount "${source}" /media/cdrom -o loop
-#echo "${source}" |egrep -o '[^/]+[.](ISO|iso)'
-#echo "${target}"/`echo "${source}" |egrep -o '[^/]+[.](ISO|iso)'`
-mkdir -p "${target}/`echo "${source}" |egrep -o '[^/]+[.](ISO|iso)'`"
+mount "${source}" /media/cdrom -o loop
+target="${target}/`echo "${source}" |egrep -o '[^/]+[.](ISO|iso)'`"
+mkdir -p "${target}"
 #rsync -av --recursive --delete -h --times --links --hard-links \
 #			--stats --progress \
 #			"${source}" "${target}"
