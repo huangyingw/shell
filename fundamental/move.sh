812d1238aaf12a810312141dac6ace7b245983ca
huangyingw@linux_programming <huangyingw@gmail.com>
2010-05-09 23:28:02 +0800

    n

--- fundamental/move.sh
+++ fundamental/move.sh
@@ -5,4 +5,7 @@ RSYNCTARGET=$2
 rsync -av --recursive -h --times --links --hard-links \
 	--stats --progress \
 	"${RSYNCSOURCE}" "${RSYNCTARGET}" \
+&& rsync -av --recursive -h --times --links --hard-links \
+	--stats --progress \
+	"${RSYNCSOURCE}" "${RSYNCTARGET}" \
 && rm -frv "${RSYNCSOURCE}"
