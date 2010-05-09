137f150843332e6e6738717f00abebeef79dc41e
huangyingw@linux_programming <huangyingw@gmail.com>
2010-05-09 10:50:11 +0800

    n

--- fundamental/iso.sh
+++ fundamental/iso.sh
@@ -4,6 +4,7 @@ source=/home/huangyingw/GBTD-001\]\[Players.GameAoi\ Sora.ISO
 target=/home/huangyingw/folder
 
 mount "${source}" /media/cdrom -o loop
+echo "${source}" |grep -o '[^/]+*[.]ISO'
 #rsync -av --recursive --delete -h --times --links --hard-links \
 #			--stats --progress \
 #			"${source}" "${target}"
--- regulator/match-files-name.sh
+++ regulator/match-files-name.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 #egrep -o '[^/]+[^1234567890]*[.]jpg' /root/myproject/linux/shell/regulator/file
-egrep -o '[^/]+[^1234567890]*[.]jpg' /root/myproject/linux/shell/regulator/file
+egrep -o '[^/]+[^1234567890]*[.]jpg' /media/volgrp/myproject/git/linux/shell/regulator/file
 
