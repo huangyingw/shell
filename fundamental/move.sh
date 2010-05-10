d7627ae876e581dbfd3dc2a9f0ffb7ec37f8b2b9
huangyingw@Console <huangyingw@gmail.com>
2010-05-10 23:32:24 +0800

    n

--- fundamental/move.sh
+++ fundamental/move.sh
@@ -2,10 +2,17 @@
 RSYNCSOURCE=$1
 RSYNCTARGET=$2
 
-rsync -av --recursive -h --times --links --hard-links \
-	--stats --progress \
-	"${RSYNCSOURCE}" "${RSYNCTARGET}" \
-&& rsync -av --recursive -h --times --links --hard-links \
-	--stats --progress \
-	"${RSYNCSOURCE}" "${RSYNCTARGET}" \
-&& rm -frv "${RSYNCSOURCE}"
+#这里的-d 参数判断$myPath是否存在
+if [ ! -d "$myPath"]; then
+mkdir "$myPath"
+fi
+
+
+
+#rsync -av --recursive -h --times --links --hard-links \
+#	--stats --progress \
+#	"${RSYNCSOURCE}" "${RSYNCTARGET}" \
+#&& rsync -av --recursive -h --times --links --hard-links \
+#	--stats --progress \
+#	"${RSYNCSOURCE}" "${RSYNCTARGET}" \
+#&& rm -frv "${RSYNCSOURCE}"
