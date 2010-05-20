f90a42ce79845c58ab9406b9e44fb307d1db3e8c
huangyingw@linux_programming <huangyingw@gmail.com>
2010-05-20 23:10:20 +0800

    n

--- fundamental/find.sh
+++ fundamental/find.sh
@@ -15,6 +15,6 @@ fi
 #find /root/myproject/linux/shell/folder/ -path "*svn" -prune -o -print > ${script_path}/find.log
 #find /usr -name \*.c -o -name "*.h" -exec grep -wnHA5 dirent {} \; > ${script_path}/find.log
 #find / -path "/media" -prune -o -name "*kdiff3*"> ${script_path}/find.log
-#find /media/myproject/git/linux/shell/ -type f -name "*jpg" > ${script_path}/find.log
 #find / \( -path  /sys -o -path /root -o -path /proc -o -path /media/smb -o -path /srv -o -path /opt \) -prune -o -exec grep -wnHA5 eclipse {} \; > ${script_path}/find.log
-find / \( -path  /sys -o -path /root -o -path /proc -o -path /media/smb -o -path /srv \) -prune -o -type d -name "NX" > ${script_path}/find.log
+#find / \( -path  /sys -o -path /root -o -path /proc -o -path /media/smb -o -path /srv \) -prune -o -type d -name "tomcat6" > ${script_path}/find.log
+find / \( -path  /sys -o -path /root -o -path /proc -o -path /media/smb -o -path /srv \) -prune -o -type f -name "server.xml" > ${script_path}/find.log
