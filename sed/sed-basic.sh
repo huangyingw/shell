90c945a486be1c58c9c4e65ba7125ae20c7a3e8c
huangyingw@linux_programming <huangyingw@gmail.com>
2010-05-21 21:57:21 +0800

    n

--- sed/sed-basic.sh
+++ sed/sed-basic.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 #把chart换成bbs 把pro换成grup 
-sed -e 's/chart/bbs/;s/pro/grup/' < /media/volgrp/myproject/git/linux/shell/sed/file
-
-#将匹配行中的--open-files-limit=替换为空, 即删除 
-echo "--open-files-limit=1234888" | sed -e "s;--open-files-limit=;;" 
-#或 
-echo "--open-file-limit=1234888"|sed -e "s/--open-file-limit=//" 
+#sed -e 's/chart/bbs/;s/pro/grup/' < /media/volgrp/myproject/git/linux/shell/sed/file
+#
+##将匹配行中的--open-files-limit=替换为空, 即删除 
+#echo "--open-files-limit=1234888" | sed -e "s;--open-files-limit=;;" 
+##或 
+#echo "--open-file-limit=1234888"|sed -e "s/--open-file-limit=//" 
+x=echo "$1" | sed -e 's/chart/bbs/;s/pro/grup/'
+echo x
