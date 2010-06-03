b1f7a3744f4e81fba31994d9076159d8a790a859
huangyingw@linux_programming <huangyingw@gmail.com>
2010-06-03 22:51:43 +0800

    n

---
+++ fundamental/if.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+IFS=$'\n'
+
+dir=/root/myproject/git/folder/
+log_file=/root/myproject/git/linux/shell/fundamental/log.txt
+
+cat $log_file | while read file ; do
+   echo "prefix${file}"
+done
+
+echo $IFS
+
+find "${dir}" -type f | while read file ; do
+   echo "prefix${file}"
+done
+
+echo $IFS
+
+find $dir -type f -exec echo prefix'{}' \;
+
+for ((i = 0;i<=5;i++))
+do
+  echo "Welcome $i times"
+done
