53dcfeff39640fd68872040808b385f5c94c5b22
huangyingw@linux_programming <huangyingw@gmail.com>
2010-06-04 23:11:44 +0800

    n

--- call/becalled.sh
+++ call/becalled.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-x=`echo $0 | grep "^/"`
-
-if test "${x}"; then
-	script_path=$(dirname "$0")
-else
-	script_path=$(dirname `pwd`/$0)
-fi
 return hello
--- call/call_shell.sh
+++ call/call_shell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 result=/root/myproject/git/linux/shell/call/becalled.sh
-echo $variabel
+echo $result
