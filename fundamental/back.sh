98ed84c0288dddc585d71d297f8815e5218879ab
huangyingw@Laptop <huangyingw@gmail.com>
2010-06-06 11:05:53 -0400

    n

--- fundamental/back.sh
+++ fundamental/back.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-tar cizvf ${script_path}/Ubuntu.9.4.64.backup.tar.gz /var/cache/apt/archives --exclude=/var/cache/apt/archives/partial/* --exclude=/var/cache/apt/archives/lock
+tar cizvf Ubuntu.10.4.64.backup.tar.gz / --exclude=/var/cache/apt/archives/partial/* --exclude=/var/cache/apt/archives/lock --exclude=/media
