ead563a80ca64be9c7bb831d1a2bf5371910866b
huangyingw@linux_programming <huangyingw@gmail.com>
2010-06-20 10:21:19 +0800

    n

--- backup/back.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-tar cvpzf backup.tgz --exclude=/proc --exclude=/var/spool/apt-mirror --exclude=/lost+found --exclude=/var/lib/mldonkey --exclude=/media --exclude=/root/backup.tgz --exclude=/mnt --exclude=/sys /
