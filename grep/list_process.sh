ac6d05bdc63319b99a3a43f0dbe1bae4c7b96019
huangyingw@linux_programming <huangyingw@gmail.com>
2010-06-29 11:03:28 -0400

    n

--- grep/list_process.sh
+++ grep/list_process.sh
@@ -1,14 +1,6 @@
 #! /bin/sh
-x=`echo $0 | grep "^/"`
 
-if test "${x}"; then
-	script_path=$(dirname "$0")
-else
-	x=`echo $0 | sed -e 's/\.\///g'`
-	script_path=$(dirname `pwd`/${x})
-fi
-
-for file in `find /root/myproject/linux/shell/folder/`
+for file in `find /root/myproject/git/linux/shell/folder/`
 do
    egrep -o ".*/\([a-z]*\).*\.jpg,\1,p" ${file}
 done
