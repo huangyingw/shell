89405e61aaf04b035ed026c7eb09997c69710396
huangyingw@linux_programming <huangyingw@gmail.com>
2011-06-23 16:14:14 +0800

    n

--- other/check_properties.sh
+++ other/check_properties.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo hello
+
+for i in `find /home/huangyingw/cvs/main/ -name "*properties"`
+do
+  if [ -f $i ] 
+  then 
+    echo "Looking in $i ..."
+    cat $i | grep -inH [a-z0-9]\{4,5\}
+    if [ $? == 0 ]
+    then
+      echo "==> Found in $i"
+    fi
+  fi  
+done
